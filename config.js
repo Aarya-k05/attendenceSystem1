c77ef18d3d36e1c9a541c3375300b38d9a7a5324
Aarya-k05 <katikaraarya@gmail.com>
2025-03-27 15:41:52 +0000

    2

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCEeWpZ-U5Ul-2crVl-p63mtXdCHl1EmJY",
+    authDomain: "attendance-c1a20.firebaseapp.com",
+    projectId: "attendance-c1a20",
+    storageBucket: "attendance-c1a20.firebasestorage.app",
+    messagingSenderId: "1038346861821",
+    appId: "1:1038346861821:web:ea7e787bd351cd4ed55294"
+  };
